e9521540005fabcdce887cbc7ab2ebb13f28c15d
Michelle <blo16803@uvg.edu.gt>
2019-03-18 17:14:23 -0600

    se agrego los datos y se agrego el .r inicial

---
+++ proyecto.R
@@ -0,0 +1,9 @@
+########################################
+## Universidad del Valle de Guatemala ##
+## Proyecto 1 Mineria de datos        ##
+## Authores:                          ##
+##    Sergio Marchena                 ##
+##    Fernando Rodriguez              ##
+##    Michelle Bloomfield             ##
+########################################
+
