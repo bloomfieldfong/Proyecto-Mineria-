73017a8d5150d2853c510bb46713d88343f1cf8d
Michelle <blo16803@uvg.edu.gt>
2019-03-20 21:57:18 -0600

    se agrego el clustering

--- proyecto.R
+++ proyecto.R
@@ -67,6 +67,19 @@ mes <- table(data$MES)
 mes
 
 
+##¿Cual es el porcentaje de menores de edad que cometen faltas? ¿Que tipo de faltas cometen?
+menores <- subset(data, data$EDAD<18, select=c("EDAD", "FALTA"))
+menores1 <- table(menores)
+menores1
+
+length(menores1)
+
+##¿Cuántos menores de edad cometen faltas por estar alcoholizados?
+Alchol <- subset(data, data$ESTADO.EBRIEDAD<2, select=c("EDAD", "ESTADO.EBRIEDAD"))
+menoresAlcohol <-subset(Alcohol, Alcohol$EDAD<18, select=c("EDAD", "ESTADO.EBRIEDAD"))
+
+nrow(menoresAlcohol)
+
 ##ESCOLARIDAD - DEPARTAMENTO
 ?cor
 correlacion <- cor(data$DEPARTAMENTO,data$ESCOLARIDAD, "pearson", use = "complete.obs")
@@ -83,3 +96,42 @@ ggscatter(data, x = "DEPARTAMENTO", y = "ESCOLARIDAD",
           xlab = "Departamento", ylab = "Grado de Escolaridad")
 
 ################################---------- CLUSTERING -------------#########################################
+
+
+
+
+dataCompleto <- data.frame(departamento = data$DEPARTAMENTO, mes = data$MES, year = data$AÃ.O, falta = data$FALTA, sexo = data$SEXO, edad = data$EDAD, etnia = data$Ã.TNIA, estadoConyugal = data$EST.CONYUGAL, departamentoNacimiento = data$DEPTO..DE.NACIMIENTO, condAlfabetismo = data$COND.ALFABETISMO, escolaridad = data$ESCOLARIDAD, ocupacionhabitual = data$OCUPACION.HABITUAL, estadoEbriedad = data$ESTADO.EBRIEDAD, area = data$AREA.GEOGRAFICA)
+
+#Metodo de ward de varianza mínima para formar la gráfica de codo
+wss <- (nrow(dataCompleto[,1:14])-1)*sum(apply(dataCompleto[,1:14],2,var))
+for (i in 2:10) 
+  wss[i] <- sum(kmeans(dataCompleto[,1:14], centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Numero de clusters",  ylab="Within grupo de sumas de cuadrados")
+
+km<-kmeans(dataCompleto[,1:14],3)
+dataCompleto$grupo<-km$cluster
+
+
+g1<- dataCompleto[dataCompleto$grupo==1,]
+prop.table(table(g1$Species))*100
+nrow(g1)
+summary(g1)
+
+g2<- dataCompleto[dataCompleto$grupo==2,]
+prop.table(table(g2$Species))*100
+summary(g2)
+
+g3<- dataCompleto[dataCompleto$grupo==3,]
+prop.table(table(g3$Species))*100
+summary(g3)
+
+plotcluster(dataCompleto[,1:14],km$cluster) #grafica la ubicaciÃ³n de los clusters
+
+
+
+
+
+
+
+
